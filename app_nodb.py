0cb34b001723c2217b19ff68db7c927a37405c8b
syedhamidulhasnainkazmi-crypto <syedhamidulhasnainkazmi@gmail.com>
2026-07-18 10:39:16 +0500

    Fix Railway deployment

--- app_nodb.py
+++ app_nodb.py
@@ -254,4 +254,4 @@ def upgrade(plan):
     return redirect(url_for('dashboard'))
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=8080)
